a112d07a14b335bbc8709987482d40a20ec382a5
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-09 16:24:14 -0500

    test

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -48,20 +48,7 @@ return array(
                     ),
                 ),
                 'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
+                
             ),
             'login' => array(
                 'type' => 'Literal',
