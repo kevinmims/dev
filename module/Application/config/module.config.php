6a70ae5f3b2221cc0e111baad753fa2fd2f91ac3
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-09 15:46:46 -0500

    test

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -77,7 +77,7 @@ return array(
     'controllers' => array(
         'invokables' => array(
             'Application\Controller\Index' => 'Application\Controller\IndexController',
-            'Application\Controller\Websocket' => 'Application\Controller\WebsocketController',
+            'Websocket\Controller\Index' => 'Websocket\Controller\WebsocketController',
         ),
     ),
     'view_manager' => array(
