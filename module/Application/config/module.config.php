aa79fe378de39b7c448856d36f4ae81c215ebee5
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-09 15:27:32 -0500

    test

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -22,6 +22,16 @@ return array(
                     ),
                 ),
             ),
+            'login' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/login',
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'websocket',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
