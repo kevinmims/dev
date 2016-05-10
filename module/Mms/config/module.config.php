592a982fc291127e8c08108fec818ff019728d8f
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-10 16:41:51 -0500

    layout

--- module/Mms/config/module.config.php
+++ module/Mms/config/module.config.php
@@ -95,16 +95,13 @@ return array(
         ),
     ),
     'view_manager' => array(
-        'controller_map' => array(
-            'Mms' => true,
-        ),
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'layout/layout'           => __DIR__ . '/../view/layout/layout-mms.phtml',
             'mms/index/index' => __DIR__ . '/../view/mms/index/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
