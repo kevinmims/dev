255a387a11b7d4ac5ec435f488843e01447ec55c
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-10 17:03:22 -0500

    test

--- module/Mms/config/module.config.php
+++ module/Mms/config/module.config.php
@@ -24,7 +24,7 @@ return array(
             ),
             
 
-            'dashbaord' => array(
+            /*'dashbaord' => array(
                 'type' => 'Literal',
                 'options' => array(
                     'route' => '/dashboard',
@@ -34,7 +34,7 @@ return array(
                     )
                 )
             ),
-
+*/
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
@@ -51,17 +51,15 @@ return array(
                 ),
                 'may_terminate' => true,
                 'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
+                    'dashbaord' => array(
+                        'type' => 'Segment',
                         'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
+                            'route' => '/dashboard',
                             'defaults' => array(
-                            ),
-                        ),
+                                'controller' => 'Mms\Controller\Dashboard',
+                                'action' => 'index'
+                            )
+                        )
                     ),
                     
                 ),
