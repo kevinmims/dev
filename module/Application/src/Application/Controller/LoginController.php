d25eef41f6543f6a654a65d2609b03830f141109
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-10 14:40:09 -0500

    login controller

--- module/Application/src/Application/Controller/LoginController.php
+++ module/Application/src/Application/Controller/LoginController.php
@@ -2,17 +2,17 @@
 // /module/Application/src/Application/Controller/AccountController.php
 
 // This is to let the application know where this controller is located.
-namespace Application\Controller;
+namespace Login\Controller;
 
 // Default Zend Abstract Controller
 use Zend\Mvc\Controller\AbstractActionController;
 
 // Your Controller Class (Name should follow the same convention as above). This also extends the default Abstract Controller.
-class AccountController extends AbstractActionController
+class LoginController extends AbstractActionController
 {
     // Keep the name as in your route's action, replacing the dash with uppercase.
     // login-page becomes loginPage, and add Action so zend knows this is the action for the specified route.
-    public function loginAction() {
+    public function indexAction() {
         // Your logic code here.        
     }
 
