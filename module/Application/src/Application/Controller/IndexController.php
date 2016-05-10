bdd0e91b55e4d165218f58dfd8dc6ff9ddfef3d1
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-10 16:19:27 -0500

    test

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -16,6 +16,7 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        $this->layout('layout/layout');
         return new ViewModel();
     }
 
