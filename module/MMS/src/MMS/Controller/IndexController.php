ced06c24c4e39cc99b2b1085d601d2e0ee1ca2b3
Kevin Mims <kevin.m@us.digitalroominc.com>
2016-05-10 14:32:24 -0500

    test

--- module/MMS/src/MMS/Controller/IndexController.php
+++ module/MMS/src/MMS/Controller/IndexController.php
@@ -7,7 +7,7 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Mms\Controller;
+namespace MMS\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
